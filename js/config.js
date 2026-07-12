cce59920aa78d22e762f9654fbe3a81669ea43de
williamrzz <williamrz078@gmail.com>
2026-07-13 03:20:40 +0700

    Update Supabase key in config.js
    Replaced placeholder Supabase key with actual key.

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 
 const SUPABASE_URL = "https://yizoloveqztkgrlxdnvq.supabase.co";
 
-const SUPABASE_KEY = "MASUKKAN_ANON_KEY_KAMU_DISINI";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlpem9sb3ZlcXp0a2dybHhkbnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MjA1MzIsImV4cCI6MjA5OTE5NjUzMn0.3nMMr8x0IS1U-KJvB6Z0QPcsr598k7jBjQYzMmBbXMA";
